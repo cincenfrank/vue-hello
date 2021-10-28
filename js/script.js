854ad0e71cc85409cec6bdedacfbca6942a04ae5
cincenfrank <fpetrangeli1@gmail.com>
2021-10-28 14:50:55 +0200

    excercise implementation

--- js/script.js
+++ js/script.js
@@ -2,7 +2,19 @@ Vue.config.devtools = true;
 window.addEventListener("DOMContentLoaded", () => {
   const vueApp = new Vue({
     el: "#root",
-    data: {},
-    methods: {},
+    data: {
+      title: "Questo è un titolo scritto con Vue",
+      image:
+        "https://d585tldpucybw.cloudfront.net/sfimages/default-source/blogs/templates/vuet-dark_870x220.png?sfvrsn=83bce1fb_3",
+      image_alt: "vue logo image",
+      image_desc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, magni esse! Consequatur aperiam quam repellendus molestias commodi iusto nisi cupiditate tempora soluta ab. Molestiae sapiente tenetur eaque rerum quae quia!",
+      last_update: new Date().toUTCString(),
+    },
+    methods: {
+      update_date() {
+        this.last_update = new Date().toUTCString();
+      },
+    },
   });
 });
